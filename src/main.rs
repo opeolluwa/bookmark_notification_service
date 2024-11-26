f663d087151ea78ac4887a12ba04493431ff0742
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2024-11-26 18:42:32 +0100

    feat: impl kafka consumer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use bookmark_adapter::bookmark_shared_adapters::email_templates::EmailBuilder;
+use kafka::consumer::{Consumer, FetchOffset, GroupOffsetStorage};
+
 fn main() {
-    println!("Hello, world!");
+    let mut consumer = Consumer::from_hosts(vec!["localhost:9092".to_owned()])
+        .with_topic_partitions("bookmark".to_owned(), &[0, 1])
+        .with_fallback_offset(FetchOffset::Earliest)
+        .with_group("bookmark_apps".to_owned())
+        .with_offset_storage(Some(GroupOffsetStorage::Kafka))
+        .create()
+        .unwrap();
+    loop {
+        for ms in consumer.poll().unwrap().iter() {
+            for m in ms.messages() {
+                println!("{:#?}", bincode::deserialize::<EmailBuilder>(m.value));
+            }
+            let _ = consumer.consume_messageset(ms);
+        }
+        consumer.commit_consumed().unwrap();
+    }
 }
+
+// kafka-topics --bootstrap-server localhost:9092 --topic bookmark --create --partitions 3 --replication-factor 1
